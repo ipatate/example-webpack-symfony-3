e2d7336c9ac5df13b04a2aee23a4d7a2a42fa139
Faramaz Pat <faramazpat@gmail.com>
2017-08-31 15:30:51 +0200

    add webpack plugin for generate manifest and icons

--- webpack-builder/webpack.prod.js
+++ webpack-builder/webpack.prod.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const webpack = require('webpack'); // eslint-disable-line
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // eslint-disable-line
 const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin'); // eslint-disable-line
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // eslint-disable-line
+const WebpackPwaManifest = require('webpack-pwa-manifest'); // eslint-disable-line
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin'); // eslint-disable-line
 
 module.exports = {
   devtool: 'source-map',
@@ -29,6 +32,53 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      ios: true,
+      inject: false,
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      theme_color: '#e81c4f',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('assets/img/icon.png'),
+          sizes: [120, 152, 167, 180, 1024],
+          destination: path.join('icons', 'ios'),
+          ios: true,
+        },
+        {
+          src: path.resolve('assets/img/icon.png'),
+          size: 1024,
+          destination: path.join('icons', 'ios'),
+          ios: 'startup',
+        },
+        {
+          src: path.resolve('assets/img/icon.png'),
+          sizes: [36, 48, 72, 96, 144, 192, 512],
+          destination: path.join('icons', 'android'),
+        },
+      ],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve('assets/img/icon.png'),
+      inject: false,
+      prefix: 'icons/',
+      icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false,
+      },
     }),
   ],
 };
